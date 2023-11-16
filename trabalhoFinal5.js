9666feb207dd891cfaf8416571e05b709cef9034
karolineoliveira456 <148974797+karolineoliveira456@users.noreply.github.com>
2023-11-16 11:18:57 -0300

    Add files via upload

---
+++ trabalhoFinal5.js
@@ -0,0 +1,25 @@
+const leitor = require("readline-sync");
+
+//Questão 1: Operações Básicas.
+//1
+let soma1 = leitor.questionInt("diga um numero: ")
+let soma2 = leitor.questionInt("diga um numero: ")
+let resultado = soma1 + soma2
+console.log(resultado);
+
+//2
+let mult1 = leitor.questionInt("diga um numero: ")
+let mult2 = leitor.questionInt("diga um numero: ")
+let resultado2 = mult1 * mult2
+console.log(resultado2);
+
+//3
+let ex1 = 10
+let ex2 = 10
+let ex3 = 2
+let resultado3 = ex1 + ex2 * ex3
+console.log(resultado3);
+
+//Questão 2: Manipulação de Strings.
+//1
+
